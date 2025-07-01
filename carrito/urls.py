9410cc818f573b8f54e15c106af84e110b562698
Rodrigo-B-R <rbetanco@outlook.es>
2025-06-30 23:22:54 -0600

    Añadir al carrito

--- carrito/urls.py
+++ carrito/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('cart', views.cart_view,name='cart')
+    path('add', views.cart_view,name='add_to_cart'),
+    # path('cart',views.cart,'cart')
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 import os
 from .models import Product
+
+#Importo los modelos y forms de carrito
+from carrito.forms import OrderItemForm
+from carrito.models import Order, OrderItem
+
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -17,11 +23,46 @@ def main_page_view(request):
     return render(request,'productos/main_page.html',context=context)
 
 
-
+@login_required
 def product_view(request,id):
+    #usa la logica de carrito para añadir productos 
+    product = get_object_or_404(Product, id=id)
 
-    product= get_object_or_404(Product, id=id)
-    context={'product':product}
+    if request.method == 'POST':
 
-    return render(request,'productos/product.html',context=context)
+        add_to_cart_form = OrderItemForm(request.POST)
 
+        if add_to_cart_form.is_valid():
+            print("Form valid:", add_to_cart_form.is_valid(), add_to_cart_form.errors)
+
+            order_item = add_to_cart_form.save(commit=False)
+            order_item.product = product
+            customer = request.user.customer
+
+            # Obtener o crear una orden activa para el usuario
+            order, created = Order.objects.get_or_create(customer=customer, complete=False)
+            print("Order:", order, "Created:", created)
+
+
+            # Buscar si ya existe un OrderItem con ese producto en la orden
+            existing_item = OrderItem.objects.filter(order=order, product=product).first()
+
+            if existing_item:
+                # Sumar cantidades si ya existe ese producto
+                existing_item.quantity += order_item.quantity
+                existing_item.save()
+            else:
+                order_item.order = order
+                order_item.save()
+
+            return redirect('add_to_cart')  # Redirige a la vista del carrito o a donde prefieras
+        else:print('Invalid Form',add_to_cart_form.errors)
+    else:
+        print('Method is not POST')
+        add_to_cart_form = OrderItemForm()
+
+    context = {
+        'product': product,
+        'form': add_to_cart_form
+    }
+    return render(request, 'productos/product.html', context)
